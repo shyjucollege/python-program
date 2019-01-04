d2607216592b7564f2286a7fe7083de7aa92a9b3
shyjucollege <43202794+shyjucollege@users.noreply.github.com>
2019-01-04 14:21:36 +0530

    Update sha.py

--- sha.py
+++ sha.py
@@ -1,9 +1,11 @@
 
 #include <stdio.h>
 
-int main(){
+int main()
+{
     int counter;
-    for(counter = 0; counter < 7; counter++){
+    for(counter = 0; counter < 7; counter++)
+    {
         printf("Hello World\n");
     }
     return 0;
