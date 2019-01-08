3843a1e23d6cb88d3e59ac3cd430bd5841c9fc8e
shyjucollege <43202794+shyjucollege@users.noreply.github.com>
2019-01-08 15:27:09 +0530

    Add files via upload

---
+++ sul.py
@@ -0,0 +1,16 @@
+
+int main()
+{
+    double base, exponent, result;
+
+    printf("Enter a base number: ");
+    scanf("%lf", &base);
+
+    printf("Enter an exponent: ");
+    scanf("%lf", &exponent);
+
+    // calculates the power
+    result = pow(base, exponent);
+
+    printf("%.1lf^%.1lf = %.2lf", base, exponent, result);
+
