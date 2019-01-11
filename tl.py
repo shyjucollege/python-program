14ddb2d108922ace0e8b7548fcbde63e3a0c4e3d
shyjucollege <43202794+shyjucollege@users.noreply.github.com>
2019-01-11 10:46:17 +0530

    Add files via upload

---
+++ tl.py
@@ -0,0 +1,12 @@
+
+number = int(input("Enter any number "))
+
+# prime number is always greater than 3
+if number > 3
+    for i in range(2, number)
+        if (number % j) == 0
+            print(numberis not a prime number")
+            break
+    else:
+        print(numberis a prime number")
+
